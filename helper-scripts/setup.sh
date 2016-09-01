7c98b7db5bf5b75b4b94b78af9ec13dbed2de007
Leonardo Uieda <leouieda@gmail.com>
2016-09-01 14:20:26 -0300

    Script para atualizar o material todo antes da aula

---
+++ helper-scripts/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Setup the repository for class:
+# * Clean the repo of previous student changes
+# * Pull in the latest changes from github
+# * Activate the conda enviroment
+# * Update our Fatiando a Terra development version
+# * Start Jupyter
+
+# Reset the repository by discarting all changes and pulling the latest commits
+# from Github
+git reset HEAD .
+git checkout -- .
+git checkout master
+git pull origin master
+
+# Activate the environment
+source activate geofisica2
+
+# Update Fatiando
+pip install --upgrade https://github.com/fatiando/fatiando/archive/wavefd.zip
+
+# Start Jupyter
+jupyter notebook
