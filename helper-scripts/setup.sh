2b33258efdc0fe94e775fe6d84463b647a8486aa
Leonardo Uieda <leouieda@gmail.com>
2016-09-02 11:45:12 -0300

    Mensagens no script de setup

--- helper-scripts/setup.sh
+++ helper-scripts/setup.sh
@@ -9,18 +9,27 @@
 
 # Reset the repository by discarting all changes and pulling the latest commits
 # from Github
+
+echo "Discarding local changes to the repository...\n\n"
 git reset HEAD .
 git checkout -- .
+
 # Erase the HDF5 caches of previous simulations
+echo "\nRemoving old simulation cache files...\n\n"
 find . -name "*.h5" -exec rm -v {} \;
+
+echo "\nFetching latest changes from Github...\n\n"
 git checkout master
 git pull origin master
 
 # Activate the environment
+echo "\nActivating conda environment...\n\n"
 source activate geofisica2
 
 # Update Fatiando
+echo "\nUpdating Fatiando a Terra...\n\n"
 pip install --upgrade https://github.com/fatiando/fatiando/archive/wavefd.zip
 
 # Start Jupyter
+echo "\nStart the notebook server...\n\n"
 jupyter notebook
